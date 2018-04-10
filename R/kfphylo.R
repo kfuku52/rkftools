5b9301aeeb65424d9e1e48765cb822021b9babb3
Kenji Fukushima <kfuku52@gmail.com>
2018-04-10 10:04:22 -0600

    bug fix, a stack size problem on recursive functions

--- R/kfphylo.R
+++ R/kfphylo.R
@@ -14,7 +14,7 @@ get_node_num_by_name = function(phy, node_name) {
 get_node_name_by_num = function(phy, node_num) {
     node_names = c(phy$tip.label, phy$node.label)
     node_nums = 1:length(node_names)
-    node_name = node_names[node_nums==node_num]
+    node_name = node_names[node_nums %in% node_num]
     return(node_name)
 }
 
@@ -28,23 +28,41 @@ get_children_num = function(phy, node_num) {
     return(children_num)
 }
 
-get_tip_labels = function(phy, node_num, out=NULL) {
-    if (is.null(out)) {
-        out = list()
-        out[['nn']] = c(node_num)
-        out[['label']] = c()
-        node_num = NULL
+get_tip_labels = function(phy, node_num, out=NULL, mode='repetitive') {
+    if (mode=='recursive') {
+        if (is.null(out)) {
+            out = list()
+            out[['nn']] = c(node_num)
+            out[['label']] = c()
+            node_num = NULL
+        }
+        if (length(out[['nn']])==0) {
+            return(out[['label']])
+        }
+        if (out[['nn']][1] <= length(phy$tip.label)) {
+            out[['label']] = c(out[['label']], phy$tip.label[out[['nn']][1]])
+        } else {
+            out[['nn']] = c(out[['nn']], get_children_num(phy, out[['nn']][1]))
+        }
+        out[['nn']] = out[['nn']][out[['nn']]!=out[['nn']][1]]
+        Recall(phy=phy, node_num=NULL, out=out)
+    } else if (mode=='repetitive') {
+        node_nums = as.integer(node_num)
+        leaf_nums = 1:length(phy$tip.label)
+        while (! all(node_nums %in% leaf_nums)) {
+            children_nums = c()
+            for (nn in node_nums) {
+                if (nn > length(phy$tip.label)) {
+                    cnums = get_children_num(phy, nn)
+                    children_nums = c(children_nums, cnums)
+                    node_nums = node_nums[node_nums!=nn]
+                }
+            }
+            node_nums = c(node_nums, children_nums)
+        }
+        tip_labels = get_node_name_by_num(phy, node_nums)
+        return(tip_labels)
     }
-    if (length(out[['nn']])==0) {
-        return(out[['label']])
-    }
-    if (out[['nn']][1] <= length(phy$tip.label)) {
-        out[['label']] = c(out[['label']], phy$tip.label[out[['nn']][1]])
-    } else {
-        out[['nn']] = c(out[['nn']], get_children_num(phy, out[['nn']][1]))
-    }
-    out[['nn']] = out[['nn']][out[['nn']]!=out[['nn']][1]]
-    Recall(phy=phy, node_num=NULL, out=out)
 }
 
 get_duplication_confidence_score = function(phy, node_num) {
